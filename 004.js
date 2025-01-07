0d9735cd624cd111525f57c1c128dda4a895767f
kasraalitabar <kasraalitabar@gmail.com>
2025-01-07 10:38:37 +0330

    15

--- 004.js
+++ 004.js
@@ -1,5 +1,5 @@
 const ad =document.getElementById("page")
-
+const hie = document.getElementById("close")
 fetch("https://jsonplaceholder.typicode.com/posts")
 .then(response => response.json())
 .then(json => renderPosts(json))
